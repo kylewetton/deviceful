bebc1b88360bce207b0ff8655c1174675e487aeb
kyleatblackfoot <kyle@blackfoot.net.nz>
2020-08-13 10:16:57 +1200

    Before core-js removal

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,14 +1,13 @@
 const Path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     app: Path.resolve(__dirname, "../src/scripts/index.js"),
   },
   output: {
-    path: Path.join(__dirname, "../build"),
+    path: Path.join(__dirname, "../dist"),
     filename: "deviceful.min.js",
     library: "Deviceful",
     libraryTarget: "umd",
@@ -23,9 +22,6 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: Path.resolve(__dirname, "../public"), to: "public" },
     ]),
-    new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, "../src/index.html"),
-    }),
   ],
   resolve: {
     alias: {
@@ -38,15 +34,6 @@ module.exports = {
         test: /\.mjs$/,
         include: /node_modules/,
         type: "javascript/auto",
-      },
-      {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[path][name].[ext]",
-          },
-        },
       },
     ],
   },
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,42 +1,26 @@
-const Path = require('path');
-const Webpack = require('webpack');
-const merge = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const common = require('./webpack.common.js');
+const Path = require("path");
+const Webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  stats: 'errors-only',
+  mode: "production",
+  devtool: "source-map",
+  stats: "errors-only",
   bail: true,
-  output: {
-    filename: 'js/[name].[chunkhash:8].js',
-    chunkFilename: 'js/[name].[chunkhash:8].chunk.js'
-  },
   plugins: [
     new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      "process.env.NODE_ENV": JSON.stringify("production"),
     }),
     new Webpack.optimize.ModuleConcatenationPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'bundle.css'
-    })
   ],
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: "babel-loader",
       },
-      {
-        test: /\.s?css/i,
-        use : [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  }
+    ],
+  },
 });
